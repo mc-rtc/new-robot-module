514d91a1099eb7f445639074e330e87d21069b3d
Pierre Gergondet <pierre.gergondet@gmail.com>
2020-11-26 10:00:42 +0800

    [cmake] Simplify test setup

--- tests/loader.in.cpp
+++ tests/loader.in.cpp
@@ -1,9 +1,13 @@
 #include <mc_rbdyn/RobotLoader.h>
 
-int main()
+int main(int argc, char * argv[])
 {
+  if(argc < 2)
+  {
+    return 1;
+  }
   mc_rbdyn::RobotLoader::clear();
-  mc_rbdyn::RobotLoader::update_robot_module_path({std::string("@ROBOT_MODULE_PATH@")});
+  mc_rbdyn::RobotLoader::update_robot_module_path({std::string(argv[1])});
   auto rm = mc_rbdyn::RobotLoader::get_robot_module(@ROBOT_MODULE_PARAMS@);
   return 0;
 }
