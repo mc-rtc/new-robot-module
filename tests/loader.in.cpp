3cd6a86585ff42c1058379c7e31498b616180ab5
Pierre Gergondet <pierre.gergondet@gmail.com>
2023-08-14 18:28:37 +0900

    Update the template
    - Remove ubuntu-18.04 from the CI
    - Remove custom clang-format scripts
    - Add pre-commit

--- tests/loader.in.cpp
+++ tests/loader.in.cpp
@@ -8,6 +8,8 @@ int main(int argc, char * argv[])
   }
   mc_rbdyn::RobotLoader::clear();
   mc_rbdyn::RobotLoader::update_robot_module_path({std::string(argv[1])});
+  // clang-format off
   auto rm = mc_rbdyn::RobotLoader::get_robot_module(@ROBOT_MODULE_PARAMS@);
+  // clang-format on
   return 0;
 }
